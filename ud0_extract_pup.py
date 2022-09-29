0accf09de763f02b4869954bdd3bddc74285fca0
olebeck <31539311+olebeck@users.noreply.github.com>
2022-09-29 02:03:51 +0200

    improve pup script

--- ud0_extract_pup.py
+++ ud0_extract_pup.py
@@ -1,19 +1,37 @@
-from io import BytesIO
 import sys
 import pup_info
 
 
+BLOCKSIZE = int(100e6)
+
 if __name__ == "__main__":
     filename = sys.argv[1]
-    output = sys.argv[2]
 
     with open(filename, "rb") as f:
-        data = f.read()
-        print(f"count: {data.count(b'SCEUF')}")
-        i = data.index(b"SCEUF")
-        with BytesIO(data[i:]) as pup:
-            info = pup_info.SCEUF.read(pup)
-            print(info)
-        pup = data[i:info.data_len]
-        with open(output, "wb") as fo:
-            fo.write(pup)
+        while True:
+            data = f.read(BLOCKSIZE)
+            if len(data) == 0 or data is None:
+                break
+
+            if data.count(b"SCEUF"):
+                print("Found a pup")
+                index = data.index(b"SCEUF")
+
+                before = f.tell()
+                real_index = before - BLOCKSIZE + index
+                print(f"at 0x{real_index:x}")
+                
+                f.seek(real_index)
+                info = pup_info.SCEUF.read(f)
+                info.print()
+                yn = input("extract?")
+                if yn != "y":
+                    f.seek(before)
+                    continue
+                else:
+                    f.seek(real_index)
+                    pup = f.read(info.data_len)
+                    with open(f"PSP2_{info.version:x}_{real_index:x}.PUP", "wb") as fo:
+                        fo.write(pup)
+                    print("written pup")
+
